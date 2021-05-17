82501b27ad3862aa1d35efdb540979b71e866899
Luis <luisfilie2101@hotmail.com>
2021-05-17 14:27:53 -0300

    adicionando a interface completa

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1 align="center">
+    eWallet
+</h1>
+<br>
+<p>
+Interface do Linkedin com o shimmer efect
+</p>
+
+<h4 align="center">
+  Linkedin
+</h4>
+
+## :rocket: Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [Node.js](https://nodejs.org/en/)
+- [React](https://reactjs.org)
+
+## 💻 Projeto
+
+App para controle de ações da minha carteira
+
+## 🤔 Como contribuir
+
+- Faça um fork desse repositório;
+- Cria uma branch com a sua feature: `git checkout -b minha-feature`;
+- Faça commit das suas alterações: `git commit -m 'feat: Minha nova feature'`;
+- Faça push para a sua branch: `git push origin minha-feature`.
+
+Depois que o merge da sua pull request for feito, você pode deletar a sua branch.
---
+++ src/components/DesktopHeader/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { 
+    Container,
+    Wrapper,
+    LinkedinIcon,
+    SearchInput,
+    HomeIcon,
+    NotificationsIcon,
+    ProfileCircle,
+    CaretDownIcon,
+ } from './styles';
+
+const DesktopHeader: React.FC = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <div className="left">
+                <LinkedinIcon />
+                <SearchInput placeholder="Pesquisar" />
+            </div>
+
+            <div className="right">
+                <nav>
+                    <button className="active">
+                        <HomeIcon />
+                        <span>Inicio</span>
+                    </button>
+                    <button>
+                        <NotificationsIcon />
+                        <span>Notificações</span>
+                    </button>
+                    <button>
+                        <ProfileCircle src="https://github.com/luisfilipebandeira.png" />
+                        <span>
+                            Eu <CaretDownIcon />
+                        </span>
+                    </button>
+                </nav>
+            </div>
+        </Wrapper>
+    </Container>  
+  );
+}
+
+export default DesktopHeader;
